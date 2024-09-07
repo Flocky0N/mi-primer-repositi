ca578d986b5bb219c2705931e225b31b43d67db7
Naidelyn <artigabaibelin@gmail.com>
2024-09-07 17:56:35 -0600

    mi primer commit

---
+++ Bono_Guia3/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace Bono_Guia3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtSueldo = new System.Windows.Forms.TextBox();
+            this.combotxt = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(96, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Empleado";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(99, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Sueldo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(99, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Categoria";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(212, 47);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 3;
+            // 
+            // txtSueldo
+            // 
+            this.txtSueldo.Location = new System.Drawing.Point(212, 95);
+            this.txtSueldo.Name = "txtSueldo";
+            this.txtSueldo.Size = new System.Drawing.Size(100, 20);
+            this.txtSueldo.TabIndex = 4;
+            // 
+            // combotxt
+            // 
+            this.combotxt.FormattingEnabled = true;
+            this.combotxt.Items.AddRange(new object[] {
+            "Gerencia",
+            "Ventas",
+            "Informatica",
+            "Administrativo"});
+            this.combotxt.Location = new System.Drawing.Point(212, 140);
+            this.combotxt.Name = "combotxt";
+            this.combotxt.Size = new System.Drawing.Size(121, 21);
+            this.combotxt.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(80, 224);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Calcular Bono";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(252, 224);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Limpiar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(435, 295);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.combotxt);
+            this.Controls.Add(this.txtSueldo);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtSueldo;
+        private System.Windows.Forms.ComboBox combotxt;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Bono_Guia3/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bono_Guia3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Declaracion de variables
+            string puesto, nombre;
+            double sueldo = 0, bono = 0, salarioTotal = 0;
+            int categoria;
+            try
+            {
+
+
+                //Capturando datos
+
+                nombre = txtNombre.Text;
+                // validando que los campos no esten vacios
+                if (string.IsNullOrEmpty(nombre))
+                {
+                    MessageBox.Show("Debe completar el nombre del empleado");
+                    return;
+                }
+                sueldo = Convert.ToDouble(txtSueldo.Text);
+
+                if (sueldo <= 0)
+                {
+                    MessageBox.Show("El salario debe ser mayor a cero");
+                    return;
+                }
+                if (combotxt.SelectedIndex == -1)
+                {
+                    MessageBox.Show("No a selecciona ninguna categoria");
+                    return;
+                }
+                else
+                {
+                    puesto = Convert.ToString(combotxt.SelectedIndex);
+                    categoria = combotxt.SelectedIndex + 1;
+                }
+                switch (categoria)
+                {
+                    case 1:
+                        bono = 500;
+                        break;
+                    case 2:
+                        bono = 300;
+                        break;
+                    case 3:
+                        bono = 200;
+                        break;
+                    case 4:
+                        bono = 100;
+                        break;
+                    default:
+                        bono = 0;
+                        break;
+                }
+                salarioTotal = bono + sueldo;
+
+                MessageBox.Show(string.Format("El nombre es: {0}", nombre));
+                MessageBox.Show(string.Format("El sueldo es: {0}", sueldo));
+                MessageBox.Show(string.Format("El puesto es: {0}", puesto));
+                MessageBox.Show(string.Format("El bono es: {0}", bono));
+                MessageBox.Show(string.Format("El salario Total es: {0}", salarioTotal));
+
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(string.Format("Mensaje: {0}", error.Message));
+            }
+
+        }
+    }
+}
